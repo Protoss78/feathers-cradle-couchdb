0ebd1d06829bff7206c8090abf2738797178ffec
Joao Araujo <joao@kapmug.com.br>
2017-06-29 11:25:58 -0300

    fix parsing of view results

--- src/index.js
+++ src/index.js
@@ -161,12 +161,15 @@ class Service {
 
                   for (let i=0,N=res.length; i<N; i++) {
 
-                        var key = res[i].key;
-                        res[i] = res[i].doc ? res[i].doc : res[i].value;
-                        res[i].id = res[i]._id;
-                        if (key) res[i].key = key
-                        delete res[i]._id;
-                        delete res[i]._rev;
+                    if(res[i]._id) res[i].id = res[i]._id;
+                    if (res[i].key) {
+                        res[i].viewData = JSON.parse(JSON.stringify(res[i]))
+                        delete res[i].key
+                        delete res[i].value
+                        delete res[i].doc
+                    }
+                    delete res[i]._id;
+                    delete res[i]._rev;
 
                       const arr = filters.$select;
                       if (arr && Array.isArray(arr) && arr.length>0) {
